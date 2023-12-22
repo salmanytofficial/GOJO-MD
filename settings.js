401d9e46355f1eeb49d556754cc61551f99dc4df
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2023-12-23 02:51:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,26 +22,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ownernomer = "923184070915" //ur owner number2
+global.ownername = "𝗚𝗢𝗝𝗢 𝗞𝗨𝗡" //ur owner name
+global.ytname = "YT: S4 SALMAN YT" //ur yt chanel name
+global.socialm = "GitHub: salmanytofficial" //ur github or insta name
+global.location = "Pakistan, Punjab, Lahore" //ur location
 
 //new
-global.botname = "Cheems Bot MD V9"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtube.com/@DGXeon"
-global.wagc = "https://chat.whatsapp.com/Kjm8rnDFcpb04gQNSTbW2d"
+global.botname = "𝙂𝙊𝙅𝙊-𝙈𝘿"
+global.ownernumber = '923184070915'
+global.ownername = '𝗚𝗢𝗝𝗢 𝗞𝗨𝗡'
+global.ownerNumber = ["923184070915@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@s4salmanyt"
+global.websitex = "https://youtube.com/@salmanopi"
+global.wagc = "https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "Kaneki Bot Inc."
+global.botscript = 'https://github.com/salmanytofficial/XLICON-MD' //script link
+global.packname = "𝙂𝙊𝙅𝙊 𝙆𝙐𝙉"
+global.author = "ᗰᗩᗪE ᗷY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
+global.creator = "923184070915@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
